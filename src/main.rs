e6cab559960dcfda8e7bde19443715f9672d0d3c
Hamid Ghadyani <hamid@hamid.cc>
2017-10-11 02:42:33 -0400

    Add a set method for tags.
    - Add accompanying tests.

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,10 @@ impl Pin {
            desc: Option<String>) -> Pin {
         Pin { url, title, tags, private, read, desc }
     }
+
+    fn set_tags(&mut self, tags: &[&str]) -> () {
+        self.tags = tags.iter().map(|s| s.to_string()).collect();
+    }
 }
 
 fn main() {
@@ -28,4 +32,26 @@ fn main() {
 
 fn print_type_of<T>(_: &T) {
     println!("{}", unsafe { std::intrinsics::type_name::<T>() });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn update_tags() {
+        let url = Url::parse( "https://githubуй.com/Здравствуйтеу" ).unwrap();
+        let mut p = Pin::new(url, "title".to_string(), vec![], true, false, None);
+
+        let tags = vec!["tag1", "tag2"];
+        p.set_tags(&tags);
+        assert_eq!(p.tags, tags);
+
+        let tags = vec![String::from("tag3"), "tag4".to_string()];
+        p.set_tags(tags
+            .iter()
+            .map(|s| s.as_str()).collect::<Vec<&str>>()
+            .as_slice());
+        assert_eq!(p.tags, tags);
+    }
 }
