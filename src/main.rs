58238ff63c7a547a0cbd26e38503e0995b44279f
Hamid Ghadyani <hamid@hamid.cc>
2017-10-13 03:40:29 -0400

    Add a method to search for a query within Pin.

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,16 @@ impl Pin {
         Pin { url, title, tags, private, read, desc }
     }
 
+    fn contains(&self, q: &str) -> bool {
+        self.url.as_ref().contains(q) ||
+            self.title.contains(q) ||
+            self.tags.iter().any(|t| t.contains(q))
+    }
+    
     fn set_tags_str(&mut self, tags: &[&str]) -> () {
         self.tags = tags.iter().map(|s| s.to_string()).collect();
     }
+
     fn set_tags(&mut self, tags: Vec<String>) -> () {
         self.tags = tags;
     }
@@ -43,7 +50,7 @@ mod tests {
 
     #[test]
     fn set_tags() {
-        let url = Url::parse( "https://githubуй.com/Здравствуйтеу" ).unwrap();
+        let url = Url::parse("https://githubуй.com/Здравствуйтеу").unwrap();
         let mut p = Pin::new(url, "title".to_string(), vec![], true, false, None);
 
         let tags = vec!["tag1", "tag2"];
@@ -51,10 +58,12 @@ mod tests {
         assert_eq!(p.tags, tags);
 
         let tags = vec![String::from("tag3"), "tag4".to_string()];
-        p.set_tags_str(tags
-            .iter()
-            .map(|s| s.as_str()).collect::<Vec<&str>>()
-            .as_slice());
+        p.set_tags_str(
+            tags.iter()
+                .map(|s| s.as_str())
+                .collect::<Vec<&str>>()
+                .as_slice(),
+        );
         assert_eq!(p.tags, tags);
 
         let tags = vec![String::from("tag5"), "tag6".to_string()];
