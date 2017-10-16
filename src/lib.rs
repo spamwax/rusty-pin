2b29debb0e72f970f681ea9f63c53273f7387578
Hamid Ghadyani <hamid@hamid.cc>
2017-10-16 04:44:22 -0400

    Move tests back to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -32,8 +32,8 @@ impl Pin {
     }
 
     pub fn contains(&self, q: &str) -> bool {
-        self.url.as_ref().contains(q) || self.title.contains(q) ||
-            self.tags.iter().any(|t| t.contains(q))
+        self.url.as_ref().contains(q) || self.title.contains(q)
+            || self.tags.iter().any(|t| t.contains(q))
     }
 
     pub fn set_tags_str(&mut self, tags: &[&str]) -> () {
@@ -45,4 +45,31 @@ impl Pin {
     }
 }
 
-mod tests;
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn set_tags() {
+        let url = Url::parse("https://githubуй.com/Здравствуйтеу").unwrap();
+        let mut p = Pin::new(url, "title".to_string(), vec![], true, false, None);
+
+        let tags = vec!["tag1", "tag2"];
+        p.set_tags_str(&tags);
+        assert_eq!(p.tags, tags);
+        p.title = "fuck".to_string();
+
+        let tags = vec![String::from("tag3"), "tag4".to_string()];
+        p.set_tags_str(
+            tags.iter()
+                .map(|s| s.as_str())
+                .collect::<Vec<&str>>()
+                .as_slice(),
+        );
+        assert_eq!(p.tags, tags);
+
+        let tags = vec![String::from("tag5"), "tag6".to_string()];
+        p.set_tags(tags.clone());
+        assert_eq!(p.tags, tags);
+    }
+}
