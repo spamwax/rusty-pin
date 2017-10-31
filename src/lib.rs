2a0b01b4ead251aa55f6c7fd07bf1c1e27f13862
Hamid Ghadyani <hamid@hamid.cc>
2017-10-31 03:51:44 -0400

    Don't duplicate 'extern crate' within internal modules.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 extern crate url_serde;
+extern crate reqwest;
 
 use url::Url;
 use chrono::prelude::*;
--- src/pinboard/api.rs
+++ src/pinboard/api.rs
@@ -1,17 +1,16 @@
 const TOKEN: &'static str = include_str!("auth_token.txt");
 
-extern crate serde;
-extern crate serde_json;
-extern crate reqwest;
+use serde_json;
+use reqwest;
 
 use chrono::prelude::*;
 use url::Url;
 
 fn add_auth_token(url: &str) -> Url {
-    Url::parse_with_params(url, &[("format", "json") , ("auth_token", TOKEN)]).unwrap()
+    Url::parse_with_params(url, &[("format", "json"), ("auth_token", TOKEN)]).unwrap()
 }
 
-fn recent_update() -> Result<DateTime<Utc>, String> {
+pub fn recent_update() -> Result<DateTime<Utc>, String> {
     use std::io::Read;
 
     #[derive(Serialize, Deserialize, Debug)]
@@ -23,9 +22,9 @@ fn recent_update() -> Result<DateTime<Utc>, String> {
     let res = reqwest::get(add_auth_token("https://api.pinboard.in/v1/posts/update"));
     let mut resp = match res {
         Ok(resp) => resp,
-        Err(e) => return Err(e.to_string())
+        Err(e) => return Err(e.to_string()),
     };
-    
+
     //TODO: check for error status codes and return them instead of panicking.
     assert!(resp.status().is_success());
 
@@ -36,8 +35,8 @@ fn recent_update() -> Result<DateTime<Utc>, String> {
 
     let date: Result<UpdateTime, _> = serde_json::from_str(&content);
     match date {
-        Ok(date) =>  Ok(date.datetime),
-        Err(e) => Err(e.to_string())
+        Ok(date) => Ok(date.datetime),
+        Err(e) => Err(e.to_string()),
     }
 }
 
