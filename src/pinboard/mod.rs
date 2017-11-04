42ee5f3d6f4c2ea4cc0f76440200749ef8dea20c
Hamid Ghadyani <hamid@hamid.cc>
2017-11-04 04:13:14 -0400

    Skeleton for PinBuilder.

--- src/pinboard/mod.rs
+++ src/pinboard/mod.rs
@@ -1,6 +1,37 @@
+use serde_json;
+use url_serde;
+use reqwest;
+use reqwest::IntoUrl;
+
+use chrono::prelude::*;
+use url::Url;
+
+use Pin;
 mod api;
 
-fn can_access_api(_x: usize) -> Option<usize> {
-    api::recent_update();
-    None
+#[derive(Debug)]
+pub struct Pinboard {
+    auth_token: String,
+}
+
+#[derive(Debug)]
+struct PinBuilder {
+    pin: Pin,
+}
+
+impl PinBuilder {
+    fn new<T: IntoUrl>(url: T, title: String) -> Self {
+        PinBuilder { pin: Pin::new(url.into_url().unwrap(), title, vec![], true, false, None) }
+    }
+}
+
+impl PinBuilder {
+    fn tags(t: String) -> Self {
+        unimplemented!();
+    }
+}
+impl Pinboard {
+    pub fn new(auth_token: String) -> Self {
+        Pinboard { auth_token }
+    }
 }
