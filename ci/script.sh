c93c53d79445cedbf3f239b4a60f141df361e2f7
Hamid Ghadyani <hamid@hamid.cc>
2018-04-15 22:14:47 -0400

    Minor changes to script.sh

--- ci/script.sh
+++ ci/script.sh
@@ -4,15 +4,15 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target "$TARGET"
 
     if [ ! -z "$DISABLE_TESTS" ]; then
+        cross build --target "$TARGET"
         return
     fi
 
     # cross test --target $TARGET
     mkdir -p ~/.cache/mockito-rusty-pin
-    # export RUST_LOG=rusty_pin=debug
+    export RUST_LOG=rusty_pin=debug
     case "$TARGET" in
         x86_64-unknown-linux-gnu)
             cargo test --target "$TARGET" -- --nocapture --test-threads=1
